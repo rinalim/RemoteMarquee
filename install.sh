27e0e7f64940540dc734ac7c79dad55952eb448e
rinalim <46272802+rinalim@users.noreply.github.com>
2018-12-31 13:57:25 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,8 +11,14 @@
 # https://github.com/zzeromin/RetroPie-OLED.git
 
 cd /home/pi/RemoteMarquee/
-cp runcommand-onstart.sh /opt/retropie/configs/all/
-cp runcommand-onend.sh /opt/retropie/configs/all/
+#cp runcommand-onstart.sh /opt/retropie/configs/all/
+#cp runcommand-onend.sh /opt/retropie/configs/all/
+sudo sed -i '/rom_name/d' /opt/retropie/configs/all/runcommand-onstart.sh 
+echo 'rom_name=$3' >> /opt/retropie/configs/all/runcommand-onstart.sh 
+echo 'rom_name="${rom_name##*/}"' >> /opt/retropie/configs/all/runcommand-onstart.sh 
+echo 'rom_name="${rom_name%.*}"' >> /opt/retropie/configs/all/runcommand-onstart.sh 
+echo 'echo "$1 $rom_name" > /tmp/remotemarquee.log' >> /opt/retropie/configs/all/runcommand-onstart.sh 
+
 #sed -i '1i\\/usr/bin/python /home/pi/RemoteMarquee/RemoteMarquee.py &' /opt/retropie/configs/all/autostart.sh
 apt-get install -y imagemagick sysstat libjpeg8-dev
 git clone https://github.com/jacksonliam/mjpg-streamer.git
