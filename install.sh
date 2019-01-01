67a2c098fa17711dcd17fe2d08cb577dc4fc73da
rinalim <46272802+rinalim@users.noreply.github.com>
2019-01-01 19:40:54 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,25 +10,27 @@
 # https://github.com/ipromiseyou/RetroPie-AutoSet.git
 # https://github.com/zzeromin/RetroPie-OLED.git
 
-cd /home/pi/RemoteMarquee/
-#cp runcommand-onstart.sh /opt/retropie/configs/all/
-#cp runcommand-onend.sh /opt/retropie/configs/all/
 sudo sed -i '/rom_name/d' /opt/retropie/configs/all/runcommand-onstart.sh 
 echo 'rom_name=$3' >> /opt/retropie/configs/all/runcommand-onstart.sh 
 echo 'rom_name="${rom_name##*/}"' >> /opt/retropie/configs/all/runcommand-onstart.sh 
 echo 'rom_name="${rom_name%.*}"' >> /opt/retropie/configs/all/runcommand-onstart.sh 
 echo 'echo "$1 $rom_name" > /tmp/remotemarquee.log' >> /opt/retropie/configs/all/runcommand-onstart.sh 
 
-#sed -i '1i\\/usr/bin/python /home/pi/RemoteMarquee/RemoteMarquee.py &' /opt/retropie/configs/all/autostart.sh
+sed -i '1i\\/bin/sh /home/pi/RemoteMarquee/stream.sh' /opt/retropie/configs/all/autostart.sh
+sed -i '1i\\/usr/bin/python /home/pi/RemoteMarquee/RemoteMarquee.py &' /opt/retropie/configs/all/autostart.sh
+
 apt-get install -y imagemagick sysstat libjpeg8-dev
+
+cd /home/pi/RemoteMarquee/
+mkdir watching
 git clone https://github.com/jacksonliam/mjpg-streamer.git
 cd mjpg-streamer/mjpg-streamer-experimental
 make
 make install
+
 chgrp -R -v pi /home/pi/RemoteMarquee
 chown -R -v pi /home/pi/RemoteMarquee
-chgrp -R -v pi /opt/retropie/configs/all
-chown -R -v pi /opt/retropie/configs/all
+
 echo "Setup Completed. Reboot after 3 Seconds."
 sleep 3
 reboot
