c96a3b11213af1da42e4dd252d42014a0f335c2f
rinalim <46272802+rinalim@users.noreply.github.com>
2019-01-07 14:33:15 +0900

    Update spi-run.py

--- client/spi-run.py
+++ client/spi-run.py
@@ -3,6 +3,8 @@
 import os
 from subprocess import *
 
+INTRO = "/home/pi/RemoteMarquee/client/intro.mp4"
+
 def run_cmd(cmd):
 # runs whatever in the cmd variable
     p = Popen(cmd, shell=True, stdout=PIPE)
@@ -10,4 +12,8 @@ def run_cmd(cmd):
     return output
 
 run_cmd("dd if=/dev/zero of=/dev/fb1")
+
+if os.path.isfile(INTRO) == True:
+    run_cmd("mplayer -vo fbdev2:/dev/fb1 -zoom -xy 480 -demuxer lavf -framedrop INTRO </dev/null >/dev/null 2>&1")
+
 run_cmd("mplayer -vo fbdev2:/dev/fb1 -zoom -xy 480 -demuxer lavf -framedrop http://127.0.0.1:8080/?action=stream </dev/null >/dev/null 2>&1")
