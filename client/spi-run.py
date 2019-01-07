a128ec29849b6548f8d0e2927ca73d866b244dcb
rinalim <46272802+rinalim@users.noreply.github.com>
2019-01-07 14:11:58 +0900

    Update spi-run.py

--- client/spi-run.py
+++ client/spi-run.py
@@ -1,2 +1,13 @@
-dd if=/dev/zero of=/dev/fb1
-mplayer -vo fbdev2:/dev/fb1 -zoom -xy 480 -demuxer lavf -framedrop http://127.0.0.1:8080/?action=stream </dev/null >/dev/null 2>&1
+#!/usr/bin/python
+
+import os
+from subprocess import *
+
+def run_cmd(cmd):
+# runs whatever in the cmd variable
+    p = Popen(cmd, shell=True, stdout=PIPE)
+    output = p.communicate()[0]
+return output
+
+run_cmd("dd if=/dev/zero of=/dev/fb1")
+run_cmd("mplayer -vo fbdev2:/dev/fb1 -zoom -xy 480 -demuxer lavf -framedrop http://127.0.0.1:8080/?action=stream </dev/null >/dev/null 2>&1")
