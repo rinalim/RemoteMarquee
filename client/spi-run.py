1e4991064245eecb9010fda699ef2f0f6711691a
rinalim <46272802+rinalim@users.noreply.github.com>
2019-01-07 14:33:47 +0900

    Update spi-run.py

--- client/spi-run.py
+++ client/spi-run.py
@@ -14,6 +14,6 @@ def run_cmd(cmd):
 run_cmd("dd if=/dev/zero of=/dev/fb1")
 
 if os.path.isfile(INTRO) == True:
-    run_cmd("mplayer -vo fbdev2:/dev/fb1 -zoom -xy 480 -demuxer lavf -framedrop INTRO </dev/null >/dev/null 2>&1")
+    run_cmd("mplayer -vo fbdev2:/dev/fb1 -zoom -xy 480 -demuxer lavf -framedrop " + INTRO + " </dev/null >/dev/null 2>&1")
 
 run_cmd("mplayer -vo fbdev2:/dev/fb1 -zoom -xy 480 -demuxer lavf -framedrop http://127.0.0.1:8080/?action=stream </dev/null >/dev/null 2>&1")
