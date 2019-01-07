aa8bc79d6b46389b361210e22ccd8e8c52e36789
rinalim <46272802+rinalim@users.noreply.github.com>
2019-01-07 14:04:08 +0900

    Update spi-setup.sh

--- client/spi-setup.sh
+++ client/spi-setup.sh
@@ -1,1 +1,4 @@
 sudo apt-get install -y mplayer
+
+sudo sed -i '/spi-run/d' /opt/retropie/configs/all/autostart.sh 
+sudo sed -i '1i\\/usr/bin/python /home/pi/RemoteMarquee/client/spi-run.py &' /opt/retropie/configs/all/autostart.sh
