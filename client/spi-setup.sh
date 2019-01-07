335588a04d2777cee23c4d43c3d8947ff9ed18da
rinalim <46272802+rinalim@users.noreply.github.com>
2019-01-07 15:23:27 +0900

    Update spi-setup.sh

--- client/spi-setup.sh
+++ client/spi-setup.sh
@@ -2,3 +2,7 @@ sudo apt-get install -y mplayer
 
 sudo sed -i '/spi-run/d' /opt/retropie/configs/all/autostart.sh 
 sudo sed -i '1i\\/usr/bin/python /home/pi/RemoteMarquee/client/spi-run.py &' /opt/retropie/configs/all/autostart.sh
+
+echo "Reboot after 3 Seconds."
+sleep 3
+reboot
