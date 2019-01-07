e36748daaf53fa7881548ecfcc137a1df81c23b6
rinalim <46272802+rinalim@users.noreply.github.com>
2019-01-07 18:56:21 +0900

    Update RemoteMarquee.py

--- RemoteMarquee.py
+++ RemoteMarquee.py
@@ -3,8 +3,12 @@
 import os
 from subprocess import *
 from time import *
+from datetime import datetime
 
-FONT_SIZE = 12
+USER = 'RiNa@RaspigamerCafe'
+INTERVAL = 1
+FONT1_SIZE = 14
+FONT2_SIZE = 14
 
 def run_cmd(cmd):
 # runs whatever in the cmd variable
@@ -12,6 +16,15 @@ def run_cmd(cmd):
     output = p.communicate()[0]
     return output
 
+def get_ip_address(cmd, cmdeth):
+    # ip & date information
+    ipaddr = run_cmd(cmd)
+
+    # selection of wlan or eth address
+    count = len(ipaddr)
+    if count == 0 :
+        ipaddr = run_cmd(cmdeth)
+    return ipaddr
 
 def get_cpu_temp():
     tempFile = open("/sys/class/thermal/thermal_zone0/temp")
@@ -19,8 +32,17 @@ def get_cpu_temp():
     tempFile.close()
     return "{0:.2f}".format(float(cpu_temp)/1000)
 
-
 def main():
+
+    #get ip address of eth0 connection
+    cmdeth = "ip addr show eth0 | grep 'inet ' | awk '{print $2}' | cut -d/ -f1"
+    #get ip address of wlan0 connection
+    cmd = "ip addr show wlan0 | grep 'inet ' | awk '{print $2}' | cut -d/ -f1"
+
+    delay = INTERVAL
+    if FONT2_SIZE != 0:
+        delay = delay - 1
+
     run_cmd('echo "maintitle" > /tmp/remotemarquee.log')
     while True:
         romfile = "maintitle"
@@ -28,7 +50,7 @@ def main():
             f = open('/tmp/remotemarquee.log', 'r')
             # except FileNotFoundError:
         except IOError:
-		print "IOError"
+            print "IOError"
         else:
             line = f.readline()
             words = line.split()
@@ -50,17 +72,26 @@ def main():
                     else:
                         romfile = "maintitle"
 
-        if FONT_SIZE == 0:
-            run_cmd("cp '/home/pi/RemoteMarquee/marquee/" + romfile + ".jpg' /home/pi/RemoteMarquee/watching/result.jpg")
-            sleep(1)
-        else:
+        run_cmd("cp '/home/pi/RemoteMarquee/marquee/" + romfile + ".jpg' /tmp/result.jpg")
+
+        if FONT1_SIZE > 0:
+            date =  datetime.now().strftime( "%b %d %H:%M" )
+            log1 =  USER + '    ' + date
+            run_cmd("convert -background black -fill white -font FreeSans -pointsize " + str(FONT1_SIZE) + " label:'" + log1 + "' /tmp/log1.png")
+            run_cmd("composite -gravity North /tmp/log1.png '/tmp/result.jpg' /tmp/result.jpg")
+        if FONT2_SIZE > 0:
+            ipaddr = get_ip_address(cmd, cmdeth)
+            ipaddr = ipaddr.replace("\n","")
             lines = run_cmd('iostat -c 1 2').split('\n')
             words = lines[6].split()
             cpu_load = "{0:.2f}".format(100 - float(words[5]))
-            log = 'CPU load = ' + cpu_load + ' Temperature = ' + get_cpu_temp() + ' '
-            run_cmd("convert -background black -fill white -font FreeSans -pointsize " + str(FONT_SIZE) + " label:'" + log + "' /tmp/log.png")
-            run_cmd("composite -gravity Southeast /tmp/log.png '/home/pi/RemoteMarquee/marquee/" + romfile + ".jpg' /home/pi/RemoteMarquee/watching/result.jpg")
-            #sleep(0)
+            log2 = 'IP: ' + ipaddr + '    CPU: ' + cpu_load + '%/' + get_cpu_temp() + u"\u2103"
+            run_cmd("convert -background black -fill white -font FreeSans -pointsize " + str(FONT2_SIZE) + " label:'" + log2 + "' /tmp/log2.png")
+            run_cmd("composite -gravity South /tmp/log2.png '/tmp/result.jpg' /tmp/result.jpg")
+        
+        run_cmd("cp /tmp/result.jpg /home/pi/RemoteMarquee/watching/result.jpg")
+        if delay != 0:
+            sleep(delay)
 
 if __name__ == "__main__":
     import sys
@@ -76,4 +107,4 @@ if __name__ == "__main__":
     # Catch the remaining exit errors
     except:
         sys.exit(0)
-
+	
