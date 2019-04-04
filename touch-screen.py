97f01c311dd49d14af9c8c043d3346fe808b2435
rinalim <46272802+rinalim@users.noreply.github.com>
2019-04-04 17:58:21 +0900

    Update touch-screen.py

--- touch-screen.py
+++ touch-screen.py
@@ -8,28 +8,20 @@ for dev in devices:
         device = dev.fn
 print device                  
 
-for event in device.read_loop():
-    if event.type == ecodes.EV_KEY:
-        #print categorize(event)
-        #print event.type, event.code, event.value
-        if event.code == ecodes.BTN_TOUCH and event.value == 1:
-            #print 'You touched me'
-            fr = open('/tmp/marquee-mode', 'r')
-            mode = fr.readline().replace('\n','')
-            fr.close()
-            fw = open('/tmp/marquee-mode', 'w')
-            if mode == '1':
-                fw.write('2')
-            elif mode == '2':
-                fw.write('1')
-            fw.close()
-            print mode
-
-'''
-import mouse
-def test():
-    print('cc')
-
-mouse.on_click(test)
-mouse.wait(mouse.RIGHT)
-'''
+if device != "":
+    for event in device.read_loop():
+        if event.type == ecodes.EV_KEY:
+            #print categorize(event)
+            #print event.type, event.code, event.value
+            if event.code == ecodes.BTN_TOUCH and event.value == 1:
+                #print 'You touched me'
+                fr = open('/tmp/marquee-mode', 'r')
+                mode = fr.readline().replace('\n','')
+                fr.close()
+                fw = open('/tmp/marquee-mode', 'w')
+                if mode == '1':
+                    fw.write('2')
+                elif mode == '2':
+                    fw.write('1')
+                fw.close()
+                print mode
